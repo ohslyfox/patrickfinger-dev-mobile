b70d97bb3f642db368ade2c9e111577eb4dee6da
ohslyfox <patricksfinger@gmail.com>
2026-01-26 18:04:34 -0800

    update deps, use pnpm

--- src/lib/components/Scene.tsx
+++ src/lib/components/Scene.tsx
@@ -1,59 +1,52 @@
-import React, { useState } from "react";
-import Sketch from "react-p5";
-import p5Types from "p5";
+import React, { useMemo } from "react";
+import { P5Canvas, P5CanvasInstance, Sketch } from "@p5-wrapper/react";
 import { ElementContainer } from "../types/p5";
 import getSceneElements from "../elements/sceneElements";
 
 interface Props {}
 
-const Scene: React.FC<Props> = (props: Props) => {
-    const [elementContainers, setElementContainers] = useState<
-        ElementContainer[]
-    >([]);
-
-    const setup = (p5: p5Types, canvasParentRef: Element) => {
-        const canvas = p5
-            .createCanvas(p5.windowWidth, p5.windowHeight)
-            .parent(canvasParentRef);
-        canvas.position(0, 0);
-        setElementContainers(getSceneElements(p5));
+const Scene: React.FC<Props> = () => {
+  const sketch: Sketch = useMemo(() => (p5: P5CanvasInstance) => {
+    let elementContainers: ElementContainer[] = [];
+    p5.setup = () => {
+      p5.createCanvas(p5.windowWidth, p5.windowHeight);
+      elementContainers = getSceneElements(p5);
     };
 
-    const draw = (p5: p5Types) => {
-        p5.background(0);
-        for (const container of elementContainers) {
-            container.display(p5);
+    p5.draw = () => {
+      p5.background(0);
+      for (const container of elementContainers) {
+        container.display(p5);
+      }
+    };
+
+    p5.windowResized = () => {
+      p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+      for (const container of elementContainers) {
+        if (container.windowResized) {
+          container.windowResized(p5);
         }
+      }
     };
 
-    const windowResized = (p5: p5Types) => {
-        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-        for (const container of elementContainers) {
-            container.windowResized(p5);
+    p5.mousePressed = () => {
+      for (const container of elementContainers) {
+        if (container.mousePressed) {
+          container.mousePressed(p5);
         }
+      }
     };
 
-    const mousePressed = (p5: p5Types) => {
-        for (const container of elementContainers) {
-            container.mousePressed(p5);
+    p5.mouseReleased = () => {
+      for (const container of elementContainers) {
+        if (container.mouseReleased) {
+          container.mouseReleased(p5);
         }
+      }
     };
+  }, []);
 
-    const mouseReleased = (p5: p5Types) => {
-        for (const container of elementContainers) {
-            container.mouseReleased(p5);
-        }
-    };
-
-    return (
-        <Sketch
-            setup={setup}
-            draw={draw}
-            windowResized={windowResized}
-            mousePressed={mousePressed}
-            mouseReleased={mouseReleased}
-        />
-    );
+  return <P5Canvas sketch={sketch} />;
 };
 
 export default Scene;
